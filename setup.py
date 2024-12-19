dc9ef2d2f4f8c136ab210b78ecbf2b3e119dbae5
Raghavendra Vaddi <vaddiraghava123@gmail.com>
2024-12-19 13:17:30 -0600

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vaddpack',  # Package name
-    version='0.2.0',   # Initial version
+    version='1.0.0',   # Initial version
     description='A simple vaddpack package',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
--- vaddpack/HelloVaddi.py
+++ vaddpack/HelloVaddi.py
@@ -1,10 +1,8 @@
-class HelloVaddi:
+def __init__(self):
+    self.name = "MyClass"
 
-    def __init__(self):
-        self.name = "MyClass"
-
-    def helloWorld(self,str):
-        return "Hello Vaddi Package :: "+ str
+def helloWorld(self,str):
+    return "Hello Vaddi Package :: "+ str
     
-    def newWelcomeMessage(self):
-        return "New Vadd Welcome package"
+def newWelcomeMessage(self):
+    return "New Vadd Welcome package"
