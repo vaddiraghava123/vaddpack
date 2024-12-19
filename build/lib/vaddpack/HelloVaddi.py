1ee2533c403e89a45d4b10974a0172054936b03d
Raghavendra Vaddi <vaddiraghava123@gmail.com>
2024-12-19 13:39:48 -0600

    missing files-1

---
+++ build/lib/vaddpack/HelloVaddi.py
@@ -0,0 +1,8 @@
+def __init__(self):
+    self.name = "MyClass"
+
+def helloWorld(self,str):
+    return "Hello Vaddi Package :: "+ str
+    
+def newWelcomeMessage(self):
+    return "New Vadd Welcome package"
