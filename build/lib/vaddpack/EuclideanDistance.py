5cb81b914e2f68781c23f07824ad002a30068b21
Raghavendra Vaddi <vaddiraghava123@gmail.com>
2024-12-28 08:42:38 -0600

    EuclideanDistance calculation -2

--- build/lib/vaddpack/EuclideanDistance.py
+++ build/lib/vaddpack/EuclideanDistance.py
@@ -1,17 +1,17 @@
 import math
-'''Calculate distance using Euclidean distance 
-distance calculation
+'''Calculate distance using Euclidean distance formula
         parameters (list1, list2 , methodType)
         example : list1 = [(2,5),(3,7),(4,8)
-        list2 = (5,10)
-        methodType = 'Old' or methodType = 'New'
+                  list2 = (5,10)
+                  methodType = 'Old' or methodType = 'New'
+                  if methodType is 'Old', means calculate step by step without using math package
         and return the output as both min and max values (min,max)
         '''
-def distance_calculation(self,list1, list2, methodType):
-    distances = [calculate_distance(point, list2, methodType) for point in list1]
+def calculate(list1, list2, methodType):
+    distances = [_calculate_distance(point, list2, methodType) for point in list1]
     return min(distances),max(distances)
 
-def calculate_distance(self,point1, point2 ,val):   
+def _calculate_distance(point1, point2 ,val):   
     if val != 'Old': 
         return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
     else:
